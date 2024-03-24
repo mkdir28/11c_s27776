fa653be4598e99c71cc46a2f35e6960be29f5569
Maryna Kamienieva <mkdirwd@gmail.com>
2024-03-24 08:16:22 +0100

    Added Task3 - Classes to a lab_3 branch

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def e_squares(start, end):
     squares = [x**2 for x in range(start, end+1)]
     return squares
 print(e_squares(1, 10))
+
+#Task3 Classes
+class SquareGenerator:
+    def e_squares(start, end):
+        squares = [x ** 2 for x in range(start, end + 1)]
+        return squares
+
+    print(e_squares(1, 10))
