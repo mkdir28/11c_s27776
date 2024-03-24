6192737bfd23b49443fa76ec429f2b1693257891
Maryna Kamienieva <mkdirwd@gmail.com>
2024-03-24 08:06:10 +0100

    Added Task2 - Functions to a lab_3 branch, is an upadate of Task1

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 #Task1 List Comprehensions
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
+#Task2 Functions
+def e_squares(start, end):
+    squares = [x**2 for x in range(start, end+1)]
+    return squares
+print(e_squares(1, 10))
